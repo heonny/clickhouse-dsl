7e7ced381f3a686b01154a2c18133a0a41ce9e3b
heonny <changsh9155@kakao.com>
2026-04-04 19:51:29 +0900

    test: add refined company-style sample query

--- src/main/java/io/github/chang/clickhousedsl/model/Expression.java
+++ src/main/java/io/github/chang/clickhousedsl/model/Expression.java
@@ -13,4 +13,44 @@ public interface Expression<T> {
     default AliasedExpression<T> as(String alias) {
         return new AliasedExpression<>(this, Identifier.of(alias));
     }
+
+    default ComparisonExpression eq(T value) {
+        return eq(Expressions.param(value, type()));
+    }
+
+    default ComparisonExpression eq(Expression<T> other) {
+        return new ComparisonExpression(this, ComparisonOperator.EQ, other);
+    }
+
+    default ComparisonExpression gt(T value) {
+        return gt(Expressions.param(value, type()));
+    }
+
+    default ComparisonExpression gt(Expression<T> other) {
+        return new ComparisonExpression(this, ComparisonOperator.GT, other);
+    }
+
+    default ComparisonExpression lt(T value) {
+        return lt(Expressions.param(value, type()));
+    }
+
+    default ComparisonExpression lt(Expression<T> other) {
+        return new ComparisonExpression(this, ComparisonOperator.LT, other);
+    }
+
+    default ComparisonExpression gte(T value) {
+        return gte(Expressions.param(value, type()));
+    }
+
+    default ComparisonExpression gte(Expression<T> other) {
+        return new ComparisonExpression(this, ComparisonOperator.GTE, other);
+    }
+
+    default ComparisonExpression lte(T value) {
+        return lte(Expressions.param(value, type()));
+    }
+
+    default ComparisonExpression lte(Expression<T> other) {
+        return new ComparisonExpression(this, ComparisonOperator.LTE, other);
+    }
 }
